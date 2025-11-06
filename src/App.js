6dfcef5c67d59618bbf5fd1099da7e341cc8eb92
preetkumar012 <pksiet84@gmail.com>
2025-11-06 11:20:54 +0530

    content added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 
 function App() {
-  
+
   return (
     <div className="App">
-    <h2>Hello Preet</h2>
-    
+      <h2>
+        Posted inBlog
+        ECI Announces Bye-Election Schedule for 8 Assembly Constituencies Across Seven States and UTs</h2>
+      <p>New Delhi, October 6, 2025:The Election Commission of India (ECI) has announced the schedule for bye-elections to fill vacancies in eight Assembly Constituencies spread across Jammu and Kashmir, Rajasthan, Jharkhand,…</p>
+
     </div>
   );
 }
